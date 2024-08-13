04e18e760473a605109d19826e5072e21de17a9d
long <longnh@inspirelab.io>
2024-08-13 20:38:08 +0700

    use old api

---
+++ test.js
@@ -0,0 +1,5 @@
+// import { YoutubeTranscript } from 'youtube-transcript';
+
+const YoutubeTranscript = require('youtube-transcript').YoutubeTranscript;
+
+YoutubeTranscript.fetchTranscript('zB0ldJl2EEM').then(console.log);
